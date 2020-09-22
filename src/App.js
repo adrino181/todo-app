8418d69d6e6a0f614b74d896ec30f5254278f61e
adrino181 <sushilcg181@gmail.com>
2020-09-22 05:43:57 +0530

    added the structrue of code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Col , Row, Form, Button} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const CreateTask = () => {
+  return <div>
+        <h2>Add task</h2>
+        <div>
+          <Form>
+            <Form.Group controlId="taskName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+            Submit
+            </Button>
+          </Form>
+        </div>
+  </div>
 }
 
-export default App;
+const TaskViewer = () => {
+  return <div>Tasks to show</div>
+}
+
+class MainList extends React.Component{
+  constructor(props) {
+   super(props);
+   this.state = {
+     todo:[]
+   };
+ }
+  render(){
+    return <main>
+    <h1 className="text-center">MY TODO</h1>
+      <Container>
+        <Row>
+          <Col>
+            <TaskViewer />
+          </Col>
+          <Col>
+            <CreateTask />
+          </Col>
+        </Row>
+      </Container>
+     </main>;
+  }
+}
+
+export default MainList;
